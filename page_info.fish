59b521860cc037336d26d14a8e44e668c5c03e23
Lucas Garron <lgarron@chromium.org>
2016-04-16 11:59:04 -0700

    Refactor with better file moving.

--- page_info.fish
+++ page_info.fish
@@ -2,182 +2,257 @@
 
 cd "$HOME/chromium/src/"
 
-set C "chrome"
+set c "chrome"
 
-set MAIN "browser/ui"
-set VIEWS "browser/ui/views"
-set COCOA "browser/ui/cocoa"
-set ANDROID "browser/ui/android"
+set main "browser/ui"
+set views "browser/ui/views"
+set cocoa "browser/ui/cocoa"
+set android "browser/ui/android"
 
-set WS "website_settings"
-set P "permission"
-set PI "page_info"
-set B "bubble"
-set BS "bubbles"
+set ws "website_settings"
+set p "permission"
+set pi "page_info"
+set pv "popup_view"
+set b "bubble"
+set bs "bubbles"
 
 ################################
 
-function find_cpp
-  ag --cpp --objcpp "$argv[1]" -l
+# OSX: Use gsed because `sed` doesn't handle word boundaries properly on OSX.
+# brew install gnu-sed
+# brew install parallel
+
+set SED_CMD "gsed"
+if not test (which gsed)
+  function gsed
+    set $argv
+  end
 end
 
-function find_gyp_gn
-  ag -G "(\.gyp|\.gypi|\.gn)\$" "$argv[1]" -l
+function sed_cpp
+  set common_match $argv[1]
+  set -e argv[1]
+  set replacement_args $argv
+
+  for file in (ag --cpp --objcpp "$common_match" -l)
+    for replacement in $replacement_args
+      echo -n "."
+      gsed --in-place="" $replacement $file
+    end
+    echo ""
+  end
+end
+
+function sed_gyp
+  set common_match $argv[1]
+  set -e argv[1]
+  set replacement_args $argv
+
+  for file in (ag -G "(\.gyp|\.gypi|\.gn)\$" "$common_match" -l)
+    for replacement in $replacement_args
+      echo -n "-"
+      gsed --in-place="" $replacement $file
+    end
+    echo ""
+  end
 end
 
 ################################
 
-function replace_cpp
-  find_cpp "$argv[1]" | xargs sed -i .bakkybackus "s#$argv[1]#$argv[2]#g"
-  find . -iname "*.bakkybackus" | xargs rm
+function folder_and_file_renames
+  set -g CPP_SED_REPLACEMENTS ""
+  set -g GYP_SED_REPLACEMENTS ""
+
+  set -g current_step "$argv[1]"
+  function move
+    if [ "$current_step" = "step1-move" ]
+      mv -v "$c/$argv[1]" "$c/$argv[2]"
+    else
+      echo "Recording" "$c/$argv[1]" "$c/$argv[2]"
+    end
+    # Leave filename as regex dots to keep it simple (there are no false positives).
+    set -g CPP_SED_REPLACEMENTS $CPP_SED_REPLACEMENTS "s#$c/$argv[1]#$c/$argv[2]#g"
+    set -g GYP_SED_REPLACEMENTS $GYP_SED_REPLACEMENTS "s#$argv[1]#$argv[2]#g"
+  end
+
+  function handleFolder
+    move "$argv[1]" "$argv[2]"
+  end
+
+  # First argument is the folder of the file.
+  function handleFile
+    move "$argv[1]"/"$argv[2]" "$argv[1]"/"$argv[3]"
+  end
+
+  handleFolder "$main/$ws" "$main/$bs"
+
+  handleFile "$main/$bs" $ws"_infobar_delegate.cc" $p"_infobar_delegate.cc"
+  handleFile "$main/$bs" $ws"_infobar_delegate.h" $p"_infobar_delegate.h"
+
+  handleFile "$main/$bs" $ws"_ui.cc" $pi"_ui.cc"
+  handleFile "$main/$bs" $ws"_ui.h" $pi"_ui.h"
+  handleFile "$main/$bs" $ws"_unittest.cc" $pi"_unittest.cc"
+  handleFile "$main/$bs" $ws"_utils.cc" $pi"_utils.cc"
+  handleFile "$main/$bs" $ws"_utils.h" $pi"_utils.h"
+  handleFile "$main/$bs" $ws".cc" $pi".cc"
+  handleFile "$main/$bs" $ws".h" $pi".h"
+
+  ################################
+
+  handleFolder "$views/$ws" "$views/$bs"
+
+  handleFile "$views/$bs" $ws"_"$pv".cc" $pi"_"$b".cc"
+  handleFile "$views/$bs" $ws"_"$pv".h" $pi"_"$b".h"
+  handleFile "$views/$bs" $ws"_"$pv"_unittest.cc" $pi"_"$b"_unittest.cc"
+
+  ################################
+
+  handleFolder "$cocoa/$ws" "$cocoa/$bs"
+
+  handleFile "$cocoa/$bs" $ws"_"$b"_controller_unittest.mm" $pi"_"$b"_controller_unittest.mm"
+  handleFile "$cocoa/$bs" $ws"_"$b"_controller.h" $pi"_"$b"_controller.h"
+  handleFile "$cocoa/$bs" $ws"_"$b"_controller.mm" $pi"_"$b"_controller.mm"
+  handleFile "$cocoa/$bs" $ws"_utils_cocoa.h" $pi"_utils_cocoa.h"
+  handleFile "$cocoa/$bs" $ws"_utils_cocoa.mm" $pi"_utils_cocoa.mm"
+
+  ################################
+
+  handleFile "$android" $ws"_popup_android.cc" $pi"_popup_android.cc"
+  handleFile "$android" $ws"_popup_android.h" $pi"_popup_android.h"
+
+  ################################
+
+  if [ "$current_step" = "step2-fiximports" ]
+    echo -n "Fixing imports..."
+    sed_cpp "$ws" $CPP_SED_REPLACEMENTS
+    sed_gyp "$ws" $GYP_SED_REPLACEMENTS
+
+    git cl format
+    echo "Done."
+  end
+
 end
 
-function replace_chrome_gyp_gn
-  cd chrome
-  find_gyp_gn "$argv[1]" | xargs sed -i .bakkybackus "s#$argv[1]#$argv[2]#g"
-  find . -iname "*.bakkybackus" | xargs rm
-  cd -
+################################
+
+function step3renameCpp
+
+  set -g CPP_SED_REPLACEMENTS ""
+
+  function addTokenReplacement
+    echo "$argv[1] -> $argv[2]"
+    set CPP_SED_REPLACEMENTS $CPP_SED_REPLACEMENTS "s#\b$argv[1]\b#$argv[2]#g"
+  end
+
+  function sed_and_reset
+    echo "Sed and reset"
+    sed_cpp "WebsiteSettings" $CPP_SED_REPLACEMENTS
+    set -g CPP_SED_REPLACEMENTS ""
+  end
+
+  ################################
+
+  addTokenReplacement "\"WebsiteSettings" "TEMP_STRING_JSDLKFJDSF"
+  sed_and_reset
+
+  ################################
+
+  # Based on:
+  #
+  #     ag --cpp --objcpp "\b[A-Za-z0-9_]*WebsiteSettings[A-Za-z0-9_]*\b" --only-matching --ignore-case --nonumbers --nofilename | sort | uniq
+  #
+  # addTokenReplacement "BuildWebsiteSettingsPatternMatchesFilter"          "BuildPageInfoPatternMatchesFilter"
+  # addTokenReplacement "FlushLossyWebsiteSettings"                         "FlushLossyPageInfo"
+  # addTokenReplacement "Java_WebsiteSettingsPopup_addPermissionSection"    "Java_PageInfoPopup_addPermissionSection"
+  # addTokenReplacement "Java_WebsiteSettingsPopup_showDialog"              "Java_PageInfoPopup_showDialog"
+  # addTokenReplacement "Java_WebsiteSettingsPopup_updatePermissionDisplay" "Java_PageInfoPopup_updatePermissionDisplay"
+  # addTokenReplacement "MatchesWebsiteSettingsPattern"                     "MatchesPageInfoPattern"
+  addTokenReplacement "MockWebsiteSettingsUI"                             "MockPageInfoUI"
+  addTokenReplacement "RecordWebsiteSettingsAction"                       "RecordPageInfoAction"
+  addTokenReplacement "RegisterWebsiteSettingsPopupAndroid"               "RegisterPageInfoPopupAndroid"
+  addTokenReplacement "ShowWebsiteSettings"                               "ShowPageInfo"
+  addTokenReplacement "ShowWebsiteSettingsBubbleViewsAtPoint"             "ShowPageInfoBubbleViewsAtPoint"
+  addTokenReplacement "SizeForWebsiteSettingsButtonTitle"                 "SizeForPageInfoButtonTitle"
+  # addTokenReplacement "WebSiteSettingsUmaUtil"                            "PageInfoUmaUtil"
+  addTokenReplacement "WebsiteSettings"                                   "PageInfo"
+  addTokenReplacement "WebsiteSettingsAction"                             "PageInfoAction"
+  addTokenReplacement "WebsiteSettingsBubbleController"                   "PageInfoBubbleController"
+  addTokenReplacement "WebsiteSettingsBubbleControllerForTesting"         "PageInfoBubbleControllerForTesting"
+  addTokenReplacement "WebsiteSettingsBubbleControllerTest"               "PageInfoBubbleControllerTest"
+  # addTokenReplacement "WebsiteSettingsInfo"                               "PageInfoInfo"
+  # addTokenReplacement "WebsiteSettingsInfoBarDelegate"                    "PageInfoInfoBarDelegate"
+  addTokenReplacement "WebsiteSettingsPopupAndroid"                       "PageInfoPopupAndroid"
+  # addTokenReplacement "WebsiteSettingsPopupView"                          "PageInfoPopupView"
+  # addTokenReplacement "WebsiteSettingsPopupViewTest"                      "PageInfoPopupViewTest"
+  # addTokenReplacement "WebsiteSettingsPopupViewTestApi"                   "PageInfoPopupViewTestApi"
+  # addTokenReplacement "WebsiteSettingsPopup_jni"                          "PageInfoPopup_jni"
+  # addTokenReplacement "WebsiteSettingsRegistry"                           "PageInfoRegistry"
+  # addTokenReplacement "WebsiteSettingsRegistryTest"                       "PageInfoRegistryTest"
+  addTokenReplacement "WebsiteSettingsTest"                               "PageInfoTest"
+  addTokenReplacement "WebsiteSettingsUI"                                 "PageInfoUI"
+  addTokenReplacement "WebsiteSettingsUIBridge"                           "PageInfoUIBridge"
+  addTokenReplacement "websiteSettingsUIBridge"                           "PageInfoUIBridge"
+
+  # Bubble
+  addTokenReplacement "WebsiteSettingsPopupView"                          "PageInfoBubble"
+  addTokenReplacement "WebsiteSettingsPopupViewTest"                      "PageInfoBubbleTest"
+  addTokenReplacement "WebsiteSettingsPopupViewTestApi"                   "PageInfoBubbleTestApi"
+
+  # Permission
+  addTokenReplacement "WebsiteSettingsInfoBarDelegate"                    "PermissionInfoBarDelegate"
+
+  sed_and_reset
+
+  ################################
+
+  addTokenReplacement "TEMP_STRING_JSDLKFJDSF" "\"WebsiteSettings"
+  sed_and_reset
+
+  ################################
+
+  # Based on:
+  #
+  #     ag --cpp --objcpp "\b[A-Za-z0-9_]*website_settings[A-Za-z0-9_]*\b" --only-matching --nonumbers --nofilename | sort | uniq
+  #
+  # addTokenReplacement "java_website_settings"              "java_page_info"
+  # addTokenReplacement "java_website_settings_pop"          "java_page_info_pop"
+  addTokenReplacement "website_settings"                   "page_info"
+  addTokenReplacement "website_settings_"                  "page_info_"
+  addTokenReplacement "website_settings_bubble_controller" "page_info_bubble_controller"
+  addTokenReplacement "website_settings_info"              "page_info_info"
+  addTokenReplacement "website_settings_info_"             "page_info_info_"
+  # addTokenReplacement "website_settings_infobar_delegate"  "page_info_infobar_delegate"
+  addTokenReplacement "website_settings_popup_android"     "page_info_popup_android"
+  addTokenReplacement "website_settings_popup_view"        "page_info_popup_view"
+  # addTokenReplacement "website_settings_registry"          "page_info_registry"
+  # addTokenReplacement "website_settings_registry_"         "page_info_registry_"
+  addTokenReplacement "website_settings_ui"                "page_info_ui"
+  addTokenReplacement "website_settings_utils"             "page_info_utils"
+  addTokenReplacement "website_settings_utils_cocoa"       "page_info_utils_cocoa"
+
+  addTokenReplacement "website_settings_infobar_delegate"  "permission_infobar_delegate"
+
+  ################################
+
+  sed_and_reset
+
 end
 
 ################################
 
-function move
-  mv -v               "$C/$argv[1]" "$C/$argv[2]"
-  set CPP_FILE_LIST $CPP_FILE_LIST $argv[2]
-
-  replace_cpp           "$C/$argv[1]" "$C/$argv[2]"
-  replace_chrome_gyp_gn "$argv[1]"    "$argv[2]"
+switch "$argv[1]"
+case "step1-move"
+  folder_and_file_renames step1-move
+case "step2-fiximports"
+  folder_and_file_renames step2-fiximports
+case "step3-rename-cpp"
+  step3renameCpp
+case "all"
+  folder_and_file_renames step1-move
+  folder_and_file_renames step2-fiximports
+  step3renameCpp
 end
 
-function moveFileInFolder
-  move "$argv[1]"/"$argv[2]" "$argv[1]"/"$argv[3]"
-end
-
-function replaceToken
-  echo "$argv[1] -> $argv[2]"
-  replace_cpp "\b$argv[1]\b" "$argv[2]"
-end
-
-################################
-
-move "$MAIN/$WS" "$MAIN/$BS"
-
-moveFileInFolder "$MAIN/$BS" $WS"_infobar_delegate.cc" $P"_infobar_delegate.cc"
-moveFileInFolder "$MAIN/$BS" $WS"_infobar_delegate.h" $P"_infobar_delegate.h"
-
-moveFileInFolder "$MAIN/$BS" $WS"_ui.cc" $PI"_ui.cc"
-moveFileInFolder "$MAIN/$BS" $WS"_ui.h" $PI"_ui.h"
-moveFileInFolder "$MAIN/$BS" $WS"_unittest.cc" $PI"_unittest.cc"
-moveFileInFolder "$MAIN/$BS" $WS"_utils.cc" $PI"_utils.cc"
-moveFileInFolder "$MAIN/$BS" $WS"_utils.h" $PI"_utils.h"
-moveFileInFolder "$MAIN/$BS" $WS".cc" $PI".cc"
-moveFileInFolder "$MAIN/$BS" $WS".h" $PI".h"
-
-################################
-
-move "$VIEWS/$WS" "$VIEWS/$BS"
-
-moveFileInFolder "$VIEWS/$BS" $WS"_popup_view.cc" $PI"_"$B".cc"
-moveFileInFolder "$VIEWS/$BS" $WS"_popup_view.h" $PI"_"$B".h"
-moveFileInFolder "$VIEWS/$BS" $WS"_popup_view_unittest.cc" $PI"_"$B"_unittest.cc"
-
-################################
-
-move "$COCOA/$WS" "$COCOA/$BS"
-
-moveFileInFolder "$COCOA/$BS" $WS"_"$B"_controller_unittest.mm" $PI"_"$B"_controller_unittest.mm"
-moveFileInFolder "$COCOA/$BS" $WS"_"$B"_controller.h" $PI"_"$B"_controller.h"
-moveFileInFolder "$COCOA/$BS" $WS"_"$B"_controller.mm" $PI"_"$B"_controller.mm"
-moveFileInFolder "$COCOA/$BS" $WS"_utils_cocoa.h" $PI"_utils_cocoa.h"
-moveFileInFolder "$COCOA/$BS" $WS"_utils_cocoa.mm" $PI"_utils_cocoa.mm"
-
-################################
-
-moveFileInFolder "$ANDROID" $WS"_popup_android.cc" $PI"_popup_android.cc"
-moveFileInFolder "$ANDROID" $WS"_popup_android.h" $PI"_popup_android.h"
-
-################################
-
-replaceToken "\"WebsiteSettings" "TEMP_STRING_JSDLKFJDSF"
-
-# Based on:
-#
-#     ag --cpp --objcpp "\b[A-Za-z0-9_]*WebsiteSettings[A-Za-z0-9_]*\b" --only-matching --ignore-case --nonumbers --nofilename | sort | uniq
-#
-# replaceToken "BuildWebsiteSettingsPatternMatchesFilter"          "BuildPageInfoPatternMatchesFilter"
-# replaceToken "FlushLossyWebsiteSettings"                         "FlushLossyPageInfo"
-# replaceToken "Java_WebsiteSettingsPopup_addPermissionSection"    "Java_PageInfoPopup_addPermissionSection"
-# replaceToken "Java_WebsiteSettingsPopup_showDialog"              "Java_PageInfoPopup_showDialog"
-# replaceToken "Java_WebsiteSettingsPopup_updatePermissionDisplay" "Java_PageInfoPopup_updatePermissionDisplay"
-# replaceToken "MatchesWebsiteSettingsPattern"                     "MatchesPageInfoPattern"
-replaceToken "MockWebsiteSettingsUI"                             "MockPageInfoUI"
-replaceToken "RecordWebsiteSettingsAction"                       "RecordPageInfoAction"
-replaceToken "RegisterWebsiteSettingsPopupAndroid"               "RegisterPageInfoPopupAndroid"
-replaceToken "ShowWebsiteSettings"                               "ShowPageInfo"
-replaceToken "ShowWebsiteSettingsBubbleViewsAtPoint"             "ShowPageInfoBubbleViewsAtPoint"
-replaceToken "SizeForWebsiteSettingsButtonTitle"                 "SizeForPageInfoButtonTitle"
-# replaceToken "WebSiteSettingsUmaUtil"                            "PageInfoUmaUtil"
-replaceToken "WebsiteSettings"                                   "PageInfo"
-replaceToken "WebsiteSettingsAction"                             "PageInfoAction"
-replaceToken "WebsiteSettingsBubbleController"                   "PageInfoBubbleController"
-replaceToken "WebsiteSettingsBubbleControllerForTesting"         "PageInfoBubbleControllerForTesting"
-replaceToken "WebsiteSettingsBubbleControllerTest"               "PageInfoBubbleControllerTest"
-# replaceToken "WebsiteSettingsInfo"                               "PageInfoInfo"
-# replaceToken "WebsiteSettingsInfoBarDelegate"                    "PageInfoInfoBarDelegate"
-replaceToken "WebsiteSettingsPopupAndroid"                       "PageInfoPopupAndroid"
-# replaceToken "WebsiteSettingsPopupView"                          "PageInfoPopupView"
-# replaceToken "WebsiteSettingsPopupViewTest"                      "PageInfoPopupViewTest"
-# replaceToken "WebsiteSettingsPopupViewTestApi"                   "PageInfoPopupViewTestApi"
-# replaceToken "WebsiteSettingsPopup_jni"                          "PageInfoPopup_jni"
-# replaceToken "WebsiteSettingsRegistry"                           "PageInfoRegistry"
-# replaceToken "WebsiteSettingsRegistryTest"                       "PageInfoRegistryTest"
-replaceToken "WebsiteSettingsTest"                               "PageInfoTest"
-replaceToken "WebsiteSettingsUI"                                 "PageInfoUI"
-replaceToken "WebsiteSettingsUIBridge"                           "PageInfoUIBridge"
-replaceToken "websiteSettingsUIBridge"                           "PageInfoUIBridge"
-
-# Bubble
-replaceToken "WebsiteSettingsPopupView"                          "PageInfoBubble"
-replaceToken "WebsiteSettingsPopupViewTest"                      "PageInfoBubbleTest"
-replaceToken "WebsiteSettingsPopupViewTestApi"                   "PageInfoBubbleTestApi"
-
-# Permission
-replaceToken "WebsiteSettingsInfoBarDelegate"                    "PermissionInfoBarDelegate"
-
-
-replaceToken "TEMP_STRING_JSDLKFJDSF" "\"WebsiteSettings"
-
-################################
-
-# Based on:
-#
-#     ag --cpp --objcpp "\b[A-Za-z0-9_]*website_settings[A-Za-z0-9_]*\b" --only-matching --nonumbers --nofilename | sort | uniq
-#
-# replaceToken "java_website_settings"              "java_page_info"
-# replaceToken "java_website_settings_pop"          "java_page_info_pop"
-replaceToken "website_settings"                   "page_info"
-replaceToken "website_settings_"                  "page_info_"
-replaceToken "website_settings_bubble_controller" "page_info_bubble_controller"
-replaceToken "website_settings_info"              "page_info_info"
-replaceToken "website_settings_info_"             "page_info_info_"
-# replaceToken "website_settings_infobar_delegate"  "page_info_infobar_delegate"
-replaceToken "website_settings_popup_android"     "page_info_popup_android"
-replaceToken "website_settings_popup_view"        "page_info_popup_view"
-# replaceToken "website_settings_registry"          "page_info_registry"
-# replaceToken "website_settings_registry_"         "page_info_registry_"
-replaceToken "website_settings_ui"                "page_info_ui"
-replaceToken "website_settings_utils"             "page_info_utils"
-replaceToken "website_settings_utils_cocoa"       "page_info_utils_cocoa"
-
-replaceToken "website_settings_infobar_delegate"  "permission_infobar_delegate"
-
-################################
-
-# Hack the following line before `diff_cmd = BuildGitDiffCmd` in `depot_tools/git_cl.py`
-#
-#     cmd.append('-sort-includes')
-#
-git cl format
 
 ################################
 
@@ -186,3 +261,5 @@ git cl format
 # - with space: "Website Settings"
 #   - ag --cpp --objcpp --java "\b[A-Za-z0-9_]*website settings[A-Za-z0-9_]*\b" --ignore-case
 # - variables (lowercase, too)
+# - constants/string IDs
+# - histogram descriptions
